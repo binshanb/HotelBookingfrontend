b44db0b9491d421f9c17776b1bdd7a94f5a95a06
binshanb <binshanb77@gmail.com>
2024-04-17 16:42:19 +0530

    backend url update

--- src/redux/slices/userslices/userApiSlice.js
+++ src/redux/slices/userslices/userApiSlice.js
@@ -1,6 +1,6 @@
 import { apiSlice } from "./apiSlice";
 const USERS_URL ='/api'
-const baseURL = "https://backs.extremehotelbooking.online";
+const baseURL = "https://backs.extremehotelbookings.online";
 // const baseURL = "http://127.0.0.1:8000";
 
 export const userApiSlice =apiSlice.injectEndpoints({
